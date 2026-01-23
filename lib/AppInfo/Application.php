69848139bc69d29386302f766531476a68e9a059
Tomas Krupicka <tomas.krupicka@master.cz>
2026-01-23 12:04:11 +0100

    first commit

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -4,17 +4,11 @@ declare(strict_types=1);
 namespace OCA\CreateExternalConversation\AppInfo;
 
 use OCA\CreateExternalConversation\Listener\LoadTalkIntegrationListener;
-use OCA\CreateExternalConversation\Service\ConversationService;
-use OCA\CreateExternalConversation\Service\SettingsService;
 use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
 use OCP\AppFramework\Http\Events\BeforeTemplateRenderedEvent;
-use OCP\Http\Client\IClientService;
-use OCP\IConfig;
-use OCP\Security\ICrypto;
-use Psr\Log\LoggerInterface;
 
 class Application extends App implements IBootstrap {
     public const APP_ID = 'create_external_conversation';
@@ -24,22 +18,6 @@ class Application extends App implements IBootstrap {
     }
 
     public function register(IRegistrationContext $context): void {
-        // Register services
-        $context->registerService(SettingsService::class, function ($c) {
-            return new SettingsService(
-                $c->get(IConfig::class),
-                $c->get(ICrypto::class)
-            );
-        });
-
-        $context->registerService(ConversationService::class, function ($c) {
-            return new ConversationService(
-                $c->get(IClientService::class),
-                $c->get(SettingsService::class),
-                $c->get(LoggerInterface::class)
-            );
-        });
-
         // Register event listeners
         $context->registerEventListener(
             BeforeTemplateRenderedEvent::class,
